283a9829c5cce1b6112146454fcba4a69bb0ab2e
ap2k23 <125471437+ap2k23@users.noreply.github.com>
2023-08-06 11:37:36 +0530

    some comment git and github

--- App.js
+++ App.js
@@ -36,3 +36,5 @@ root.render(parent); // render te parent to my root
 // git branch -M main {create a default branch main i.e master}
 // git remote add origin https://github.com/ajay-CaTi/react.git
 // git push -u origin main(push all code in repository)
+
+// Git is a version control system that allows developers to track changes in their code. GitHub is a web-based hosting service for git repositories. In simple terms, you can use git without Github, but you cannot use GitHub without Git.
