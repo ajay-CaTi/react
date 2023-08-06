cc309cf58e931a96b9b2d67cdd72fffe76dfe82b
ap2k23 <125471437+ap2k23@users.noreply.github.com>
2023-08-06 21:45:43 +0530

    different way of importing component i.e Component Composition

--- App.js
+++ App.js
@@ -15,22 +15,52 @@
 // WHAT EVER IS ROOT IS REPLACE BY ROOT.RENDER() FUNCTION
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { className: "mm" }, "i am heading"),
-    React.createElement("h2", { className: "mm" }, "i am h2 heading"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { className: "mm" }, "i am heading"),
-    React.createElement("h2", { className: "mm" }, "i am h2 heading"),
-  ]),
-]);
 
-const jsxHeading = <h1 id="heading">Ram ram bhai</h1>;
-// JSX - is HTML like syntax but not HTML
+const Title = () => (
+  <h1 className="head" tabIndex={"1"}>
+    Hello Earth How are you
+  </h1>
+);
+
+// React function component
+const HeadingComponent = () => {
+  // Normal JS function that return jsx code that is functional component
+  return (
+    <div className="container">
+      {/* <Title /> */}
+      {/* <Title></Title> */}
+      {Title()}
+      <h1>Hello Earth How are you!</h1>
+    </div>
+  );
+};
+
+// Code Redable:- Because of JSX
+// WHat makes React code faster
+// ANs is WebPack Parcel the are builders
+
+// Attack through Browser is known as Cross-side scripting [ JSX take care of INJECTION ATTACKS prevet from cross-site scripting / JSX FIRST sanitise data and then run ]
+
+// Component Composition :- Using one Componenet to another
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(parent);
-root.render(jsxHeading); // render te parent to my root
+// root.render(heading);
+root.render(<HeadingComponent />);
+
+// React.createElement==>ReactElement(Object) ==> HTMLElement(By render)
+
+// JSX => BABEL is transpile to React.createElement ==> converted to ReactELement(Object) ==> HTMLElement(By render)
+
+// react and jsx both are different
+
+// TIME E3 45.00
+
+// create abstract syntax tree, parsing, tokens
+
+// The tabindex attribute specifies the tab order of an element (when the "tab" button is used for navigating).
+
+// JSX - is HTML like syntax but not HTML
+// render te parent to my root
 // .render() Job is :- TAke this object and create h1 tag i.e understandable by browser and put it inside root
 
 // echo "# react" >> README.md
@@ -42,3 +72,9 @@ root.render(jsxHeading); // render te parent to my root
 // git push -u origin main(push all code in repository)
 
 // Git is a version control system that allows developers to track changes in their code. GitHub is a web-based hosting service for git repositories. In simple terms, you can use git without Github, but you cannot use GitHub without Git.
+
+// JSX:- (code is transpiled before it reaches the JS  Engine) - PARCEL{from BABEL}
+
+// BABEL:- JS Compiler ( takes jsx and conv into code that Browser understand )
+
+// React.createElement==>ReactElement(Object) ==> HTMLElement(By render)
