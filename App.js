ceb552743d615db0f0affefe39f7f04d7ca91cdc
ap2k23 <125471437+ap2k23@users.noreply.github.com>
2023-08-06 11:33:40 +0530

    some comment added

--- App.js
+++ App.js
@@ -31,8 +31,8 @@ root.render(parent); // render te parent to my root
 
 // echo "# react" >> README.md
 // git init
-// git add README.md
+// git add .{add all files needs to go inside commit}
 // git commit -m "first commit"
-// git branch -M main
+// git branch -M main {create a default branch main i.e master}
 // git remote add origin https://github.com/ajay-CaTi/react.git
-// git push -u origin main
+// git push -u origin main(push all code in repository)
