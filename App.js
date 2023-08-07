e610be16453f9691f5aa37a0733f379832c9303b
ap2k23 <125471437+ap2k23@users.noreply.github.com>
2023-08-07 11:34:45 +0530

    send array through props it prints without comma ,just do array.join(,) this will comma seperated

--- App.js
+++ App.js
@@ -16,25 +16,103 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => (
-  <h1 className="head" tabIndex={"1"}>
-    Hello Earth How are you
-  </h1>
-);
-
-// React function component
-const HeadingComponent = () => {
-  // Normal JS function that return jsx code that is functional component
+const Header = () => {
   return (
-    <div className="container">
-      {/* <Title /> */}
-      {/* <Title></Title> */}
-      {Title()}
-      <h1>Hello Earth How are you!</h1>
+    <div className="header">
+      <div className="logo_container">
+        <img
+          className="logo"
+          src="https://i.pinimg.com/originals/b3/e5/f3/b3e5f321d4aedcf983a6c95f5a6d0b88.png"
+          alt="logo_img"
+        />
+      </div>
+      <div className="nav_items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   );
 };
 
+const RestaurantCard = (props) => {
+  // const {name}=props;
+  const { pageTitle, ogDescription, quzines } = props.name;
+  return (
+    <div className="res_card">
+      <h3>MaheshWar Foods</h3>
+      <h3>{pageTitle}, dal chawal, kadhi, roti</h3>
+      <h3>{quzines.join(", ")}, dal chawal, kadhi, roti</h3>
+      <h3>{ogDescription}, dal chawal, kadhi, roti</h3>
+    </div>
+  );
+};
+
+// Config Driven UI:- Web app is driven by DATA [How the UI Look Like USING DATA && and config comes from backend [it is the latest prectice that every company follows] Even for every city tere will be different data shon on every city/state]
+
+// PROPS are just normal arguement to a function
+
+// Good senior front end developeer Engineer is very good in UI and DATA LAyer Engineer
+
+const data = {
+  name: "search",
+  pageTitle: "Restaurants in Delhi NCR - Zomato",
+  pageDescription:
+    "Check out the list of all Restaurants in Delhi NCR. Check their menu, reviews & rating, photos, price, location, cuisine, offers, and more.",
+  pageUrl:
+    "/ncr/restaurants?place_name=Ywca, 1, Ashoka Rd, Hanuman Road Area, Connaught Place, New Delhi, Delhi",
+  title: "Restaurants in Delhi NCR - Zomato",
+  isNoIndex: false,
+  ogTitle: "Restaurants in Delhi NCR - Zomato",
+  ogDescription:
+    "Check out the list of all Restaurants in Delhi NCR. Check their menu, reviews & rating, photos, price, location, cuisine, offers, and more.",
+  ogPageUrl:
+    "/ncr/restaurants?place_name=Ywca, 1, Ashoka Rd, Hanuman Road Area, Connaught Place, New Delhi, Delhi",
+  subType: "delivery",
+  quzines: ["some", "thing", "good", "alwayas", "there", "be", "positive"],
+  isMobile: 0,
+  isTablet: 0,
+  isOAuthV2Enabled: false,
+  useAuthSdkForLogin: true,
+  useAuthSdkForLogout: false,
+  gaPageType: "Search",
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res_container">
+        <RestaurantCard name={data} />
+        {/* <RestaurantCard name={data} />
+        <RestaurantCard name={data} />
+        <RestaurantCard name={data} /> */}
+      </div>
+    </div>
+  );
+};
+
+// React function component
+const AppLayout = () => {
+  // Normal JS function that return jsx code that is functional component
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
+
+// style{{backgroundColor:"#f0f0f0"}}
+// first curley braces for some js code and other is JS OBJECT
+
+// root.render(heading);
 // Code Redable:- Because of JSX
 // WHat makes React code faster
 // ANs is WebPack Parcel the are builders
@@ -42,10 +120,6 @@ const HeadingComponent = () => {
 // Attack through Browser is known as Cross-side scripting [ JSX take care of INJECTION ATTACKS prevet from cross-site scripting / JSX FIRST sanitise data and then run ]
 
 // Component Composition :- Using one Componenet to another
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
-root.render(<HeadingComponent />);
 
 // React.createElement==>ReactElement(Object) ==> HTMLElement(By render)
 
